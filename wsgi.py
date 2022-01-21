72fba917858f85ab82375522ad9ba9c2c6a89f13
cindy <mianalaloharano@gmail.com>
2022-01-21 16:09:33 +0300

    added sentry

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,22 @@
 from flask import Flask, jsonify
+import os
+
+sentry_dsn = os.getenv('SENTRY_DSN', None)
+if sentry_dsn:
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+
+    sentry_sdk.init(
+        sentry_dsn,
+        integrationsb= [FlaskIntegration()]
+    )
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return jsonify({'roll':4})
+
+@app.route('/error')
+def error():
+    return {}
