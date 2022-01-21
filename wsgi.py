096cc274b4fa16ad60874e397c3058526c65edda
cindy <mianalaloharano@gmail.com>
2022-01-21 14:22:08 +0300

    from master

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-# this comment is for a conflict
+# this comment is for a conflict form master
 @app.route('/')
 def home():
     return jsonify({'roll':0})
