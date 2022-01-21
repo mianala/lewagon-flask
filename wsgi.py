309cddc289a3a7388930cbe639b5b9ace6531165
cindy <mianalaloharano@gmail.com>
2022-01-21 14:21:29 +0300

    ongoing conflict

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-# this comment is for a conflict
+# this comment is for a from branch for conflict
 @app.route('/')
 def home():
     return jsonify({'roll':0})
